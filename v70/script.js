48eb0be0394f353d7613b85515fec412cdc82818
vinaysoni1 <vinaysoni9174@gmail.com>
2025-12-20 23:39:03 +0530

    ain

---
+++ v70/script.js
@@ -0,0 +1,16 @@
+console.log("exercise 12 js file is linked");
+let boxes = document.getElementsByClassName("box");
+//console.log(boxes);
+
+function getrandomcolor() {
+    let  val1 = Math.ceil(0 + Math.random()* 255)
+    let  val2 =  Math.ceil(0 + Math.random()* 255)
+    let  val3 = Math.ceil(0 + Math.random()* 255)
+    return `rgb(${val1}, ${val2}, ${val3})`;
+}
+
+Array.from(boxes).forEach(e=> {
+e.style.backgroundColor = getrandomcolor();
+   e.style.Color = getrandomcolor();
+
+})
