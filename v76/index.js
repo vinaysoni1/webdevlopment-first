555107001a23fb26efcad5d54ea02be37abdb530
vinaysoni1 <vinaysoni9174@gmail.com>
2025-12-30 21:27:17 +0530

    Async/Await & Fetch API

---
+++ v76/index.js
@@ -0,0 +1,35 @@
+//  async function getdata(){
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() =>{
+//             resolve(455);
+//     }, 5000)
+//     });
+// }
+
+async function getdata(){
+    let x = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    let data = await x.json();
+    // console.log(data);
+    // console.log(x);
+    return data;
+}
+
+async function main(){
+console.log("Loading Modules");    
+
+console.log("Do Something else");
+
+console.log("Load Data");
+
+let Data = await getdata()
+console.log(Data);
+console.log("Process Data");
+console.log("Task 2");
+}
+main();
+
+// Data.then((value) =>{
+// console.log(Data);
+// console.log("Process Data");
+// console.log("Task 2");
+// });
