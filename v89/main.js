0f3a9da91bb8c763055350e6522642d18c86a6ea
vinaysoni1 <vinaysoni9174@gmail.com>
2026-04-10 20:17:04 +0530

    Response, Request and Routers in Express

--- v89/main.js
+++ v89/main.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const blog = require('./router/blog')
+const shop = require('./router/shop')
+
+
 const app = express()
 const port = 3000
 
 app.use(express.static("public")) // Middleware to parse JSON bodies
-
+app.use('/blog', blog)
+app.use('/shop', shop)
 // app.get('/', (req, res) => {
 //     console.log("GET request received")
 //   res.send('Hello World Get!')
@@ -11,30 +16,25 @@ app.use(express.static("public")) // Middleware to parse JSON bodies
 
 
 app.post('/', (req, res) => {
-    console.log("POST request received")
+  console.log("POST request received")
   res.send('Hello World Post!')
 })
 
 app.put('/', (req, res) => {
-    console.log("Put request received")
+  console.log("Put request received")
   res.send('Hello World Put!')
 })
 
 
 app.get('/server', (req, res) => {
-    console.log("Hey its server")
-  res.sendFile('Templates/server.html', {root: __dirname})
+  console.log("Hey its server")
+  res.sendFile('Templates/server.html', { root: __dirname })
 })
 
 
 app.get('/api', (req, res) => {
-    
-  res.json({ name: "vinay soniapp.get('/server', (req, res) => {
-    console.log("Hey its server")
-  res.sendFile('Templates/server.html', {root: __dirname})
-})", age: 19, city: "jabalpur" })
+  res.json({ a: 1, b: 2, c: 3, d: 4, name: ["harry", "jerry"] })
 })
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
