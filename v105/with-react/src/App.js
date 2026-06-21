83ddb04f781aa1bb6a7cf988edff33ee9ea7d971
vinaysoni1 <vinaysoni9174@gmail.com>
2026-06-22 00:38:22 +0530

    Introduction to React

--- v105/with-react/src/App.js
+++ v105/with-react/src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import navBar from "./components/NavBar";
+import NavBar from './components/NavBar';
+
+
 
 function App() {
+  const [value,setvalue] = useState(0)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>
+      <div className="count">{value}</div>
+      <button onClick={()=>{setvalue(value+1)}}>Click Me</button>
     </div>
   );
 }
---
+++ v105/with-react/src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <div >
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact Us</li>
+        <li>Tools</li>
+      </ul>
+    </div>
+  )
+}
+
+export default NavBar
