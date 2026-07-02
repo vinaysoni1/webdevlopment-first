5b45ae95126341e3df2c244e8303516b0f281125
vinaysoni1 <vinaysoni9174@gmail.com>
2026-07-03 00:14:49 +0530

    Exercise-18 solution are completed using secong api

--- v111/src/App.jsx
+++ v111/src/App.jsx
@@ -1,3 +1,39 @@
+// import {useState,useEffect} from 'react'
+
+// const App = () => {
+//   const [card, setcard] = useState([])
+
+
+//   const fetchdata = async () => {
+//     let a = await fetch('https://jsonplaceholder.typicode.com/posts')
+//     let b = await a.json()
+//     setcard(b)
+//     console.log(b)
+//   }
+
+//   useEffect(() => {
+//     fetchdata()
+//   }, [])
+//   return (
+//     <div className="conatiner">
+//       {card.map((card)=>{
+//         return(
+//           <div key={card.userId} className="card">
+//             <h2>UserId: {card.userId}</h2>
+//             <p>Id: {card.id}</p>
+//             <p>Title: {card.title}</p>
+//             <span>Body: {card.body}</span>
+//           </div>
+//         )
+//       })}
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
 import {useState,useEffect} from 'react'
 
 const App = () => {
@@ -5,7 +41,7 @@ const App = () => {
 
 
   const fetchdata = async () => {
-    let a = await fetch('https://jsonplaceholder.typicode.com/posts')
+    let a = await fetch('https://jsonplaceholder.typicode.com/photos')
     let b = await a.json()
     setcard(b)
     console.log(b)
@@ -19,10 +55,11 @@ const App = () => {
       {card.map((card)=>{
         return(
           <div key={card.userId} className="card">
-            <h2>UserId: {card.userId}</h2>
+            <h2>UserId: {card.albumId}</h2>
             <p>Id: {card.id}</p>
             <p>Title: {card.title}</p>
-            <span>Body: {card.body}</span>
+            <span>Body: {card.url}</span>
+            <span>Body: {card.thumbnailUrl}</span>
           </div>
         )
       })}
