2c66ed140766165b520a97171360d8f17b2d2e10
vinaysoni1 <vinaysoni9174@gmail.com>
2026-07-15 22:09:56 +0530

    edit btn complete

--- v113/src/App.jsx
+++ v113/src/App.jsx
@@ -29,13 +29,20 @@ function App() {
     setTudo(e.target.value)
   }
 
-  const HandleEdit = () => {
-    alert("edit")
+  const HandleEdit = (e,id) => {
+    let t = tudos.filter(i=>i.id===id)
+    setTudo(t[0].tudo)
+
+    let Newtudos =  tudos.filter((item=>{
+    return item.id!==id
+   }))
+  
+  setTudos(Newtudos)
   }
 
   
   const handleDelete = (e,id) => {
-    
+
    let Newtudos =  tudos.filter((item=>{
     return item.id!==id
    }))
@@ -52,20 +59,25 @@ function App() {
         <div className="addtudo">
           <h2 className='text-lg font-bold'>Add A Tudos</h2>
           <input onChange={handleChange} value={tudo} type="text" className='border bg-white border-gray-300 rounded-md p-2'/>
-          <button onClick={handleAdd} className='bg-blue-500 text-white font-semibold px-6 py-2  hover:bg-blue-900 m-8 rounded-md' >Add</button>
+          <button onClick={handleAdd} className='bg-blue-500 text-white font-semibold px-6 py-2  hover:bg-blue-900 m-8 rounded-md' >Save</button>
         </div>
 
         <h2 className='text-lg font-bold'>Your Tudos</h2>
 
-        <div className="Tudos">
+        <div className="Tudos ">
+
+          {tudos.length===0 && <div className=' text-lg font-semibold'>No Tudos Found</div>}
          {tudos.map(item=>{
          
           return<div key={item.id} className="Tudo flex w-full justify-between">
+
+            <div className="flex items-center gap-2  mt-0">
             <input name={item.id} onChange={handlecheckbox} type='checkbox' value={item.isCompleted}  id='' />
 
             <div className={item.isCompleted? "line-through" : ""}>{item.tudo}</div>
+            </div>
               <div className="btn">
-                <button onClick={HandleEdit}  className='bg-blue-500 text-white font-semibold px-6 py-2  hover:bg-blue-900 m-8 rounded-md'>Edit</button>
+                <button onClick={(e)=>HandleEdit(e,item.id)}  className='bg-blue-500 text-white font-semibold px-6 py-2  hover:bg-blue-900 m-8 rounded-md'>Edit</button>
                 <button onClick={(e)=>{handleDelete(e,item.id)}}  className='bg-blue-500 text-white font-semibold px-6 py-2  hover:bg-blue-900 m-8 rounded-md'>Delete</button>
               </div>
             </div>
