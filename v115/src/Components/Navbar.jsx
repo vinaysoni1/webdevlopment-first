417e9c225c3c8b31777af7f9e6c13ceb4082a454
vinaysoni1 <vinaysoni9174@gmail.com>
2026-07-22 00:04:55 +0530

    React Router complte

--- v115/src/Components/Navbar.jsx
+++ v115/src/Components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
        <NavLink className={(e)=> {return e.isActive? "red": ""}} to='/Home'> <li>Home</li></NavLink>
        <NavLink className={(e)=> {return e.isActive? "red": ""}} to='/About'> <li>About</li></NavLink>
        <NavLink className={(e)=> {return e.isActive? "red": ""}} to='/Login'> <li>Login</li></NavLink>
-       <NavLink className={(e)=> {return e.isActive? "red": ""}} to='/User'> <li>User</li></NavLink>
+       <NavLink className={(e)=> {return e.isActive? "blue": ""}} to='/User'> <li>User</li></NavLink>
       </nav>
     </div>
   )
