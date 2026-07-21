f7cce536a9efcef64f11c3d3600895c5554bf61f
vinaysoni1 <vinaysoni9174@gmail.com>
2026-07-21 23:58:00 +0530

    color click property

--- v115/src/Components/Navbar.jsx
+++ v115/src/Components/Navbar.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router'
+import { NavLink } from 'react-router'
 
 const Navbar = () => {
   return (
     <div>
       
       <nav>
-       <Link to='/Home'> <li>Home</li></Link>
-       <Link to='/About'> <li>About</li></Link>
-       <Link to='/Login'> <li>Login</li></Link>
-       <Link to='/User'> <li>User</li></Link>
+       <NavLink className={(e)=> {return e.isActive? "red": ""}} to='/Home'> <li>Home</li></NavLink>
+       <NavLink className={(e)=> {return e.isActive? "red": ""}} to='/About'> <li>About</li></NavLink>
+       <NavLink className={(e)=> {return e.isActive? "red": ""}} to='/Login'> <li>Login</li></NavLink>
+       <NavLink className={(e)=> {return e.isActive? "red": ""}} to='/User'> <li>User</li></NavLink>
       </nav>
     </div>
   )
