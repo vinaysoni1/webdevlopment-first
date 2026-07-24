69da851badbfda24f799fbb114da531faa91e696
vinaysoni1 <vinaysoni9174@gmail.com>
2026-07-24 23:00:35 +0530

    UseContext Hooks In react

---
+++ v116/src/Components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Component1 from './Component1'
+
+const Button = () => {
+  return (
+    <div>
+        <Button><span><Component1/></span>I am Button</Button>
+    </div>
+  )
+}
+
+export default Button
